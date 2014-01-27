86572800d4eca9b2f54433c59f142953579f551d
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-01-27 21:59:56 +0000

    Initial checkin for a message header interface.

---
+++ header.h
@@ -0,0 +1,21 @@
+/*
+Project maqet.
+Copyright (C) 2014, Roman Fakhrazeyev, <roman.fakhrazeyev@two718.com>
+*/
+
+#ifndef MAQET_HEADER_H
+#define MAQET_HEADER_H
+
+/*
+A message header.
+*/
+struct header {
+    unsigned char type: 4;
+    unsigned char dup: 1;
+    unsigned char qos: 2;
+    unsigned retain: 1;
+    unsigned char size;
+};
+
+#endif
+
