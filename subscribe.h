eab5c88d89f5acb11257855f742f0a6a87214a14
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-01-31 00:20:22 +0000

    Added a subscribe command message initial interface.

---
+++ subscribe.h
@@ -0,0 +1,16 @@
+/*
+Project maqet.
+Copyright (C) 2014, Roman Fakhrazeyev, <roman.fakhrazeyev@two718.com>
+*/
+
+#ifndef MAQET_SUBSCRIBE_H
+#define MAQET_SUBSCRIBE_H
+
+/*
+A subscribe command.
+*/
+struct connect {
+};
+
+#endif
+
