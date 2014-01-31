e5357dce77e10a23dca832768f21053d596e100a
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-01-31 00:18:41 +0000

    Added a connect command message initial interface.

---
+++ connect.h
@@ -0,0 +1,16 @@
+/*
+Project maqet.
+Copyright (C) 2014, Roman Fakhrazeyev, <roman.fakhrazeyev@two718.com>
+*/
+
+#ifndef MAQET_CONNECT_H
+#define MAQET_CONNECT_H
+
+/*
+A connect command.
+*/
+struct connect {
+};
+
+#endif
+
