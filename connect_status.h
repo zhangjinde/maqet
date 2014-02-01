afc6227587f842d60fbc4e13d456b701911372b1
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-01 16:06:18 +0000

    Added a connection status enumeration.

---
+++ connect_status.h
@@ -0,0 +1,18 @@
+/*
+Project maqet.
+Copyright (C) 2014, Roman Fakhrazeyev, <roman.fakhrazeyev@two718.com>
+*/
+
+#ifndef MAQET_CONNECT_STATUS_H
+#define MAQET_CONNECT_STATUS_H
+
+/*
+Connection return codes.
+*/
+enum connect_status {
+    accepted = 0,
+    refused_version = 1
+};
+
+#endif
+
