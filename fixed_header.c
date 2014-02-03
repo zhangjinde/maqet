61b54846ebaac51ee97848739a0794d74f9a64b8
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-03 22:21:11 +0000

    Added an initial implementation of the fixed header.

---
+++ fixed_header.c
@@ -0,0 +1,20 @@
+/*
+Project maqet.
+Copyright (C) 2014, Roman Fakhrazeyev, <roman.fakhrazeyev@two718.com>
+*/
+
+/*
+The fixed header implementation.
+*/
+
+#include <stdio.h>
+#include <assert.h>
+#include "fixed_header.h"
+
+struct fixed_header* fixed_header_read(FILE* stream) {
+    assert(stream);
+    assert(!ferror(stream));
+    assert(!ftell(stream));
+    return NULL;
+}
+
