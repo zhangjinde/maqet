b5b4522aff46e201c4484294fc8c0527ccc74974
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-03 22:20:31 +0000

    Modified an initial implementation of the protocol.

--- protocol.c
+++ protocol.c
@@ -9,10 +9,12 @@ The protocol implementation.
 
 #include <assert.h>
 #include "protocol.h"
+#include "fixed_header.h"
 
 void process(FILE* stream) {
     assert(stream);
     assert(!ferror(stream));
-    if(!ftell(stream)) rewind(stream);
+    if(ftell(stream)) rewind(stream);
+    struct fixed_header* fixed_header = fixed_header_read(stream);
 }
 
