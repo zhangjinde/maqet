b19d7e0d0d2849c4c9ea560afa49e7b9464bf83c
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-03 15:27:20 +0000

    Added an initial interface for a variable header.

---
+++ var_header.h
@@ -0,0 +1,18 @@
+/*
+Project maqet.
+Copyright (C) 2014, Roman Fakhrazeyev, <roman.fakhrazeyev@two718.com>
+*/
+
+#ifndef MAQET_VAR_HEADER_H
+#define MAQET_VAR_HEADER_H
+
+/*
+A variable header.
+*/
+struct var_header {
+    char* protocol_name;
+    unsigned char protocol_version;
+};
+
+#endif
+
