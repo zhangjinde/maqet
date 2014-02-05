46a0a5ba33f2b9d0630992996af5f8bedf6a825d
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-05 00:21:14 +0000

    Augmented the fixed header implementation.

--- fixed_header.c
+++ fixed_header.c
@@ -13,12 +13,14 @@ The fixed header implementation.
 #include "fixed_header.h"
 #include "message_type.h"
 
+static const unsigned char fixed_header_size = 2;
+
 const struct fixed_header* fixed_header_read(FILE* stream) {
     assert(stream);
     assert(!ferror(stream));
     assert(!ftell(stream));
 
-    char buffer[2];
+    char buffer[fixed_header_size];
     size_t bytes_read = fread(buffer, sizeof buffer[0], sizeof(buffer), stream);
     // TODO: error, if bytes read is not equal 2.
     // TODO: error, if buffer[0] not in [1..15] range.
