006263a777e01207473dd0242f3b99e1f2935558
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-05 20:31:26 +0000

    Completed the initial implementation of the fixed header; most error checking missing.

--- fixed_header.c
+++ fixed_header.c
@@ -24,6 +24,10 @@ void fixed_header_read(FILE* stream, struct fixed_header* fixed_header) {
     size_t bytes_read = fread(buffer, sizeof buffer[0], sizeof(buffer), stream);
     // TODO: error, if bytes read is not equal 2.
     // TODO: error, if buffer[0] not in [1..15] range.
-    fixed_header->message_type = (enum message_type)(buffer[0] >> 4);
+    fixed_header->message_type = (enum message_type)((buffer[0] >> 4) & 0xf);
+    fixed_header->dup = (buffer[0] >> 3) & 0x1;
+    fixed_header->qos = (enum qos)((buffer[0] >> 1) & 0x3);
+    fixed_header->retain = buffer[0] & 0x1;
+    fixed_header->remaining_size = buffer[1];
 }
 
