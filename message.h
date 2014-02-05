3df4793bcd0a2c86a42870d38caea82ae42dfad3
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-05 00:16:20 +0000

    Added an initial message interface.

---
+++ message.h
@@ -0,0 +1,20 @@
+/*
+Project maqet.
+Copyright (C) 2014, Roman Fakhrazeyev, <roman.fakhrazeyev@two718.com>
+*/
+
+#ifndef MAQET_MESSAGE_H_
+#define MAQET_MESSAGE_H
+
+/*
+The message interface.
+*/
+
+struct message {
+    struct fixed_header* fixed_header;
+    struct var_header* var_header;
+    struct payload* payload;
+}
+
+#endif
+
