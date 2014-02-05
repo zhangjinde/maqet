84978250a9d2e2bb8dba07153035f402ca3823f7
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-05 20:26:07 +0000

    Completed the initial interface of the fixed header.

--- fixed_header.h
+++ fixed_header.h
@@ -20,6 +20,7 @@ struct fixed_header {
     bool dup;
     enum qos qos;
     bool retain;
+    unsigned char remaining_length;
 };
 
 void fixed_header_read(FILE* stream, struct fixed_header* fixed_header);
