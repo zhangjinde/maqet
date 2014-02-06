20f2e7f0e4b9751828025100d288faa13ed03615
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-06 17:30:12 +0000

    Added more fields in the variable header.

--- var_header.h
+++ var_header.h
@@ -7,12 +7,20 @@ Copyright (C) 2014, Roman Fakhrazeyev, <roman.fakhrazeyev@two718.com>
 #define MAQET_VAR_HEADER_H
 
 /*
-A variable header.
+A variable header structires and functions.
 */
+
 struct var_header {
     char* protocol_name;
     unsigned char protocol_version;
+    enum connect_flag connect_flag;
+    unsigned short keep_alive;
+    enum connect_status connect_status;
+    char* topic_name;
+    unsigned short message_id;
 };
+
+void var_header_read(FILE* stream, struct var_header* var_header);
 
 #endif
 
