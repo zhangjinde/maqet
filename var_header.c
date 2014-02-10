5a7432456caa6b0ae46705c5d54aaac61aeb9e44
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-10 02:34:51 +0000

    Modified the variable header implementation.

--- var_header.c
+++ var_header.c
@@ -10,19 +10,17 @@ The variable header implementation.
 #include <stdio.h>
 #include <assert.h>
 #include "var_header.h"
+#include "string.h"
 
-static const char* protocol_name = u8"MQIsdp";
+static const char* protocol_name = "MQIsdp";
 static const unsigned char protocol_version = 0x03;
 
 void var_header_read(FILE* stream, struct var_header* var_header) {
     assert(stream);
     assert(!ferror(stream));
-    assert(!ftell(stream));
     assert(var_header);
 
-    char buffer[12];
-    size_t bytes_read = fread(buffer, sizeof buffer[0], sizeof(buffer), stream);
     // TODO: complete the implementation.
-    //var_header->protocol_name = buffer[0] >> 4 & 0xf;
+    string_read(stream, var_header->protocol_name);
 }
 
