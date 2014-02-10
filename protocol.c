862fc5fd86b01d391ed761b3b887ed6641015fed
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-10 17:58:52 +0000

    Modified the protocol implementation.

--- protocol.c
+++ protocol.c
@@ -18,12 +18,10 @@ void protocol_message_read(FILE* stream, struct message* message) {
     assert(!ferror(stream));
 
     if(ftell(stream)) rewind(stream);
-    struct fixed_header* fixed_header = malloc(sizeof(fixed_header));
-    fixed_header_read(stream, fixed_header);
-    struct var_header* var_header = malloc(sizeof(var_header));
-    var_header_read(stream, var_header);
+    message->fixed_header = malloc(sizeof(struct fixed_header));
+    fixed_header_read(stream, message->fixed_header);
+    message->var_header = malloc(sizeof(struct var_header));
+    var_header_read(stream, message->var_header);
     // TODO: etc...
-    free(fixed_header);
-    free(var_header);
 }
 
