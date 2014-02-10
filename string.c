469e6f22b78ebfa1368c8c16c64ce0a7d1ec31ed
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-10 02:34:24 +0000

    Added an initial MQTT string implementation.

---
+++ string.c
@@ -0,0 +1,26 @@
+/*
+Project maqet.
+Copyright (C) 2014, Roman Fakhrazeyev, <roman.fakhrazeyev@two718.com>
+*/
+
+/*
+The MQTT string decoding and encoding routines's implementation.
+*/
+
+#include <stdlib.h>
+#include <assert.h>
+#include "string.h"
+
+void string_read(FILE* stream, char* string) {
+    assert(stream);
+    assert(!ferror(stream));
+
+    unsigned char length_buffer[2];
+    // TODO: take care of the interrupts.
+    size_t bytes_read = fread(length_buffer, sizeof length_buffer[0], sizeof(length_buffer), stream);
+    unsigned short length = length_buffer[0] << 8 | length_buffer[1];
+    string = malloc(sizeof(char) * length + 1);
+    // TODO: take care of the interrupts.
+    bytes_read = fread(string, sizeof string[0], sizeof(string), stream);
+}
+
