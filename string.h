1c6827a22a18df24585f14df974fc37b687375ef
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-10 17:59:27 +0000

    Modified the MQTT string interface.

--- string.h
+++ string.h
@@ -26,7 +26,7 @@ Reads an MQTT string from the stream.
 @stream:
 @string:
 */
-void string_read(FILE* stream, char* string);
+const char* string_read(FILE* stream);
 
 /*
 Write an MQTT string to the stream.
