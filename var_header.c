898a7647059b5a4d50c47d8376f98011e0259cfe
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-11 21:01:35 +0000

    Modified the MQTT variable header implementation.

--- var_header.c
+++ var_header.c
@@ -12,6 +12,7 @@ An MQTT variable header implementation.
 #include <assert.h>
 #include "var_header.h"
 #include "string.h"
+#include "connect_attr.h"
 
 //static const char* protocol_name = "MQIsdp";
 static const unsigned char protocol_version = 0x03;
@@ -25,6 +26,7 @@ void var_header_free(struct var_header* var_header) {
 
     free(var_header->topic_name);
     free(var_header->protocol_name);
+    free(var_header->connect_attr);
     free(var_header);
 }
 
@@ -35,6 +37,9 @@ void var_header_read(FILE* stream, struct var_header* var_header) {
 
     var_header->protocol_name = string_read(stream);
     var_header->protocol_version = fgetc(stream); 
+
+    var_header->connect_attr = connect_attr_new(); 
+    connect_attr_read(stream, var_header->connect_attr);
     // TODO: complete the implementation.
 }
 
