56f8bf8c5022c0bc06aa53709ab7132bbefbc4e8
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-13 02:47:53 +0000

    Completed the implementation of the MQTT Connect Message.

--- connect_message.c
+++ connect_message.c
@@ -11,6 +11,7 @@ The MQTT Connect Message interface implementation.
 #include <stdlib.h>
 #include <assert.h>
 #include "connect_message.h"
+#include "string.h"
 
 struct connect_message* connect_message_new() {
     return malloc(sizeof(struct connect_message));
@@ -19,6 +20,11 @@ struct connect_message* connect_message_new() {
 void connect_message_free(struct connect_message* connect_message) {
     assert(connect_message);
 
+    free(connect_message->client_id);
+    free(connect_message->topic_name);
+    free(connect_message->message);
+    free(connect_message->username);
+    free(connect_message->password);
     free(connect_message);
 }
 
@@ -27,6 +33,10 @@ void connect_message_read(FILE* stream, struct connect_message* connect_message)
     assert(!ferror(stream));
     assert(connect_message);
 
-    // TODO: etc...
+    connect_message->client_id = string_read(stream);
+    connect_message->topic_name = string_read(stream);
+    connect_message->message = string_read(stream);
+    connect_message->username = string_read(stream);
+    connect_message->password = string_read(stream);
 }
 
