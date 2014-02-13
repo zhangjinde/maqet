2853158d1ec88cff7f58b9d6984ddafc3ac5e923
Roman Fakhrazeyev <roman.fakhrazeyev@two718.com>
2014-02-13 03:06:25 +0000

    Modified the MQTT Message type names and added comments.

--- message_type.h
+++ message_type.h
@@ -7,24 +7,30 @@ Copyright (C) 2014, Roman Fakhrazeyev, <roman.fakhrazeyev@two718.com>
 #define MAQET_MESSAGE_TYPE_H
 
 /*
-A message type.
+The MQTT Message type.
 */
 enum message_type {
-    unknown = 0,
-    connect = 1,
-    connection_ack = 2,
-    publish = 3,
-    publish_ack = 4,
-    publish_received = 5,
-    publish_release = 6,
-    publish_complete = 7,
-    subscribe = 8,
-    subscribe_ack = 9,
-    unsubscribe = 10,
-    unsubscribe_ack = 11,
-    ping_request = 12,
-    ping_response = 13,
-    disconnect = 14
+    /* An unknown message type. */
+    unknown = 0x00,
+
+    /* A client requests a connection to a server. */ 
+    connect = 0x01,
+
+    /* A server acknowledges a connection request. */
+    connect_ack = 0x02,
+
+    publish = 0x03,
+    publish_ack = 0x04,
+    publish_received = 0x05,
+    publish_release = 0x06,
+    publish_complete = 0x07,
+    subscribe = 0x08,
+    subscribe_ack = 0x09,
+    unsubscribe = 0x0a,
+    unsubscribe_ack = 0x0b,
+    ping_request = 0x0c,
+    ping_response = 0x0d,
+    disconnect = 0x0f
 };
 
 #endif
